16354d482a6e8a3f6d89895bf8b2c8c9634d0b9b
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-02-02 13:55:39 -0200

    Update dashaviso.user.js

--- dashaviso.user.js
+++ dashaviso.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Aviso Dashboard N1
-// @version      1.2 Beta
+// @version      1.1 Beta
 // @description  Aviso no Windows de quantos defeitos possuem na fila do N1
 // @author       Thauã Corrêa Martins, Eduardo Czamanski Rota
 // @source       https://github.com/correamth/dashn1
@@ -8,6 +8,6 @@
 // @grant        GM_notification
 // @grant        window.focus
 // @require      https://raw.githubusercontent.com/correamth/dashn1/master/sistemaenome.js
-// @require      https://raw.githubusercontent.com/correamth/dashn1/master/teste.js
+// @require      https://raw.githubusercontent.com/correamth/dashn1/master/dashn1.js
 // @icon         https://forum.vivo.com.br/attachment.php?attachmentid=737&d=1418039499
 // ==/UserScript==
