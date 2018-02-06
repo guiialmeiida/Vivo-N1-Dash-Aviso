38d3bec8ffa89f1e3e4777c648d467dc39a940d5
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-02-06 15:56:29 -0200

    Update dashavisoambientes.user.js

--- dashavisoambientes.user.js
+++ dashavisoambientes.user.js
@@ -4,7 +4,7 @@
 // @version      1.1 Beta
 // @description  Aviso no Windows de quantos defeitos possuem na fila de Ambientes
 // @author       Thauã Corrêa Martins, Eduardo Czamanski Rota
-// @source       https://github.com/correamth/dashn1
+// @source       https://github.com/correamth/dashaviso
 // @match        http://svuxqsoa113:9010/*
 // @match        http://10.41.252.111:9010/*
 // @grant        GM_notification
