bbc0266b2003f04f0143bf2f9bde696234ea6c04
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-02-15 11:26:24 -0200

    Update sistemaenome.js

--- sistemaenome.js
+++ sistemaenome.js
@@ -1,3 +1,4 @@
 var seunome = "Seu Nome";
 var sistemasx = ["sistema 1", "sistema 2", "sistema 3"]; 
 var temporefresh = 30000;
+var opcionalcores = "yes"; //APENAS PARA N1. Yes se você quer as cores na sua dashboard, ficará preto sempre que tiver um defeito em aberto e o SLA vermelho quando tiver maior que 20min ou 1hs
