198987b2f7a16018a3cfbcd136dd937ac75734a4
Eduardo Czamanski Rota <kaname.pf@gmail.com>
2018-02-19 13:13:55 -0300

    Update sistemaenome.js

--- sistemaenome.js
+++ sistemaenome.js
@@ -3,6 +3,7 @@ var sistemasx = ["sistema 1", "sistema 2", "sistema 3"];
 var temporefresh = 50000;
 
 var avisoOpcional = "yes"; //Yes para os avisos na área de trabalho
+var avisoOpcionalMeusDefeitos = "yes"; //Yes para os avisos na área de trabalho
 var opcionalcores = "yes"; //APENAS PARA N1. Yes para mudança de layout
 var corbg = "background-color:black";
 var fonte = "color:white";
