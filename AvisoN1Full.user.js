1c0b8aca399495a1fa6b0837ebffa4ed49f2871d
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-03-07 13:55:03 -0300

    Create AvisoN1Full.user.js

---
+++ AvisoN1Full.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Aviso Dashboard Ambientes
+// @version      2.0 Beta
+// @description  Notificação dos defeitos, opções layout
+// @author       Thauã Corrêa Martins, Eduardo Czamanski Rota
+// @source       https://github.com/correamth/dashaviso
+// @match        http://svuxqsoa113:9010/*
+// @match        http://10.41.252.111:9010/*
+// @grant        GM_notification
+// @grant        window.focus
+// @require      https://raw.githubusercontent.com/correamth/dashaviso/master/AvisoN1Full.js
+// @icon         https://forum.vivo.com.br/attachment.php?attachmentid=737&d=1418039499
+// ==/UserScript==
