e4201b02ff9f020bc1383ca14551795d37741ca9
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-10-03 15:34:02 -0300

    Update AvisoN1Full.user.js

--- AvisoN1Full.user.js
+++ AvisoN1Full.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Aviso Dashboard
-// @version      2.4 Beta
+// @version      2.5 x
 // @description  Notificação dos defeitos, opções layout
 // @author       Thauã Corrêa Martins, Eduardo Czamanski Rota
 // @source       https://github.com/correamth/dashaviso
